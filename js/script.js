95ed6d2ceeb507e6afa6411409f7a15aaf9fdc46
michelecanini <studio.michelecanini@gmail.com>
2023-05-18 23:56:30 +0200

    [UPDATE] milestone 1 done

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
 // FORM CALCULATING THE PRICE OF A TRAIN TIKET
 
-// INSERT CLICK BUTTON SUBMIT-GENERA
-let submit = document.getElementById('submit');
-submit.addEventListener ('click', function () {
-
-    let km_travel = document.getElementById('km_travel').value;
-console.log(km_travel);
-
-    let age_passenger = document.getElementById('age_passenger').value;
-console.log(age_passenger);
-});
-
 // INSERT VARIABLE PRICE KM, PRICE MINORS END PRICE OVER65
 let price_km = 0.21;
 console.log (price_km);
@@ -18,3 +7,25 @@ let price_minors = 0.21 / 5 * 4;
 console.log (price_minors);
 let price_over = 0.21 / 5 * 3;
 console.log (price_over);
+
+// INSERT CLICK BUTTON SUBMIT-GENERA
+let button = document.getElementById('submit');
+
+button.addEventListener ('click', function () {
+
+    let km_travel = document.getElementById('km_travel').value;
+
+    let age_passenger = document.getElementById('age_passenger').value;
+    console.log (parseInt(age_passenger));
+
+    if ((age_passenger) <= 17){
+        document.getElementById('price_tiket').innerHTML += `${(km_travel * price_minors).toFixed(2)}` + "€";
+
+    } else if ((age_passenger >= 18) && (age_passenger < 65)){ 
+        document.getElementById('price_tiket').innerHTML += `${(km_travel * price_km).toFixed(2)}` + "€";
+
+    }else if (age_passenger >= 65){
+        document.getElementById('price_tiket').innerHTML += `${(km_travel * price_over).toFixed(2)}` + "€";
+    }
+});
+
