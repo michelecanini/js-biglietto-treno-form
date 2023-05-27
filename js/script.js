9842cbf2258e2d52cd5f0a79b90c6e3af22cc489
michelecanini <studio.michelecanini@gmail.com>
2023-05-27 18:16:10 +0200

    [UPDATE] adjustment name end lastname html

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-// FORM CALCULATING THE PRICE OF A TRAIN TIKET
+// PROGRAMMA PER IL CALCOLO DEL PREZZO DEL BIGLIETTO
 
-// INSERT VARIABLE PRICE KM, PRICE MINORS END PRICE OVER65
+// INSERIMENTO COSTANTE DEL PREZZO STANDARD AL KM
 const price_km = 0.21;
+
+// INSERIMENTO VARIABILE PREZZO PER MINORI -20%
 let price_minors = 0.21 / 5 * 4;
+
+// INSERIMENTO PREZZO OVER65 -40%
 let price_over = 0.21 / 5 * 3;
 
-// INSERT NAME END LASTNAME
-//let name_complete = document.getElementById('name_complete').value;
-//console.log (name_complete);
-
-// INSERT CLICK BUTTON SUBMIT-GENERA END CONDITIONAL IF
+// INSERIMENTO DEL BOTTON SUBMIT-GENERA E DELLA FUNZIONE CLICK
 let button = document.getElementById('submit');
-
 button.addEventListener ('click', function () {
 
+    // INSERIMENTO VARIABILI DEI KM DI VIAGGIO E DELL'ETA DEL PASSEGGERO
     let km_travel = parseInt(document.getElementById('km_travel').value);
     let age_passenger = parseInt(document.getElementById('age_passenger').value);
 
@@ -31,4 +31,9 @@ button.addEventListener ('click', function () {
         console.log(km_travel * price_over.toFixed(2));
     }
     document.getElementById('price_tiket').innerText = message;
+
+    // VARIABILE ED INSERIMENTO NOME E COGNOME PASSEGGERO
+    let name_complete = document.getElementById('name_complete').value;
+    document.getElementById('hello_user').innerHTML = `${name_complete}`;
+    
 });
